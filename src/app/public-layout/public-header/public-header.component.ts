5a0dfffa821d6f01b5409301c1a09c40f89a63d7
orozco92 <goldstein.david92@gmail.com>
2022-10-22 10:56:18 -0400

    Header menu items. Navigate to services

--- src/app/public-layout/public-header/public-header.component.ts
+++ src/app/public-layout/public-header/public-header.component.ts
@@ -11,6 +11,36 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
 export class PublicHeaderComponent {
     logoUrl = '';
     companyName = AppConstants.companyName;
+    menuItems = [
+        {
+            url: '/',
+            label: 'Inicio',
+            fragment: 'home'
+        },
+        {
+            url: '/',
+            label: 'Servicios',
+            fragment: 'features'
+        },
+        {
+            url: '/',
+            label: 'Highlights',
+            fragment: 'highlights'
+        },
+        {
+            url: '/',
+            label: 'Pricing',
+            fragment: 'pricing'
+        },
+        {
+            url: '/admin',
+            label: 'Administracion',
+        },
+        {
+            url: '/testing',
+            label: 'Testing',
+        }
+    ]
     constructor(public layoutService: LayoutService, public router: Router) {
         // this.logoUrl = `assets/layout/images/${layoutService.config.colorScheme === 'light' ? 'logo-dark' : 'logo-white'}.svg`;
         this.logoUrl = 'assets/images/logo.png';
