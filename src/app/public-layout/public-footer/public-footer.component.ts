03d9628f11b2edd67e10d9427dfa3e938d35186c
orozco92 <goldstein.david92@gmail.com>
2022-11-15 06:51:34 -0500

    Company info at public footer Fixes #2

--- src/app/public-layout/public-footer/public-footer.component.ts
+++ src/app/public-layout/public-footer/public-footer.component.ts
@@ -11,6 +11,11 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
 export class PublicFooterComponent {
     logoUrl = '';
     companyName = AppConstants.companyName;
+    address = 'Calle Maceo # 191 e/ Cables y Ángel Guerra, Holguín. CP 80100';
+    phone = '+53 24471113';
+    email = 'some@email.com';
+    fbUrl = 'https://www.facebook.com/archivohistorico.provincialdeholguin';
+    twitterUrl = 'https://twitter.com/holguinarchivo';
     constructor(public layoutService: LayoutService, public router: Router) {
         this.logoUrl = 'assets/images/logo.png';
     }
